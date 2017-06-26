717cd33c960923f007a09ba0804efe9da67b479d
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-06-26 10:32:14 +0200

    prepare 3.0.38

--- xhtml2pdf/version.py
+++ xhtml2pdf/version.py
@@ -17,7 +17,7 @@
 __reversion__ = "$Revision: 247 $"
 __author__    = "$Author: holtwick $"
 __date__      = "$Date: 2008-08-15 13:37:57 +0200 (Fr, 15 Aug 2008) $"
-__version__   = VERSION = "VERSION{3.0.37}VERSION"[8:-8]
+__version__   = VERSION = "VERSION{3.0.38}VERSION"[8:-8]
 __build__     = BUILD   = "BUILD{2010-06-16}BUILD"[6:-6]
 
 VERSION_STR = """XHTML2PDF/pisa %s (Build %s)
